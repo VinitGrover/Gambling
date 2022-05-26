9e820cfe47c54c21dbe4c421222561f2c293235b
VinitGrover <vinitgrover255@gmail.com>
2022-05-26 12:21:18 +0530

    Added uc1

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -3,5 +3,7 @@ package com.bridgelabz;
 public class GamblingSimulator {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator Program");
+
+        System.out.println("Gambler puts 100$ everyday and for every game if gambler wins there is a reward of 1$ and if lost 1$ will be deducted from the stake of 100$");
     }
 }
